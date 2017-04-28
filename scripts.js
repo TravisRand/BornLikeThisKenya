c0744ac64f0ed38df268ddfb3d3baf1839e0a9b5
TravisRand <travislrand@gmail.com>
2017-04-28 19:20:12 +0000

    first commit

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// JavaScript File
+
+// JavaScript for the navigation bar
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
